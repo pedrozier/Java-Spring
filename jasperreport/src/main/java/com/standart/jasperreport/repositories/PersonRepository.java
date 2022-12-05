5ba3a837add1669d349e419e810ab736afbf24ad
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2022-12-05 12:11:05 -0300

    Delete jasperreport directory

--- jasperreport/src/main/java/com/standart/jasperreport/repositories/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.standart.jasperreport.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.standart.jasperreport.models.PersonModel;
-
-public interface PersonRepository extends JpaRepository<PersonModel, Long> {
-
-}
